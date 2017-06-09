e9ae1787c9879c4efb4d84d2b6e829bbec5b93c6
Nana <tietje.hh@gmail.com>
2017-06-09 18:38:51 +0200

    redcube komprimiert

--- Assets/Scripts/RedCube.cs
+++ Assets/Scripts/RedCube.cs
@@ -4,361 +4,263 @@ using UnityEngine;
 
 //-------------------------------------------------------
 
-public class RedCube : MonoBehaviour {
-
-	////////////////////////////
-	// Alle Variablen
-
-	// Öffentliche Floatvariablen für das maximale Ziehen und Drücken des Cubes + die Geschwindigkeit + die Kraft die ausgeübt auf der Kugel
-	public float maxPull = 0;
-	public float maxPush = 0;
-	public float speed = 1f;
-	public float force = 0f;
-
-	// Öffentliche Intvariable für das Defenieren der Position des Cubes (0 = Boden; 1 = Decke; 2 = Vorne; 3 = Hinten; 4 = Links; 5 = Rechts)
-	public int location = 0;
-
-	// Bool zum Prüfen ob der Cube allgemein eine Kollision mit Objekten haben soll
-	public bool haveCollision = false;
-	
-	// Startposition, Endposition und Distanz
-	private Vector3 startPos;
-	private Vector3 endPos;
-	private float distance = 1f;
-
-	// Bool zum Überprüfen ob die Animation läuft und ob die Kollision aktiv oder inaktiv ist
-	private bool isBusy = false;
-	private bool collisionOn = false;
-
-//-------------------------------------------------------
-
-	////////////////////////////
-	// Use this for initialization
-	void Start () {
-
-		startPos = transform.position;
-
-	}
-
-//-------------------------------------------------------
-
-	////////////////////////////
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-//-------------------------------------------------------
-
-//////////////////////////////////////////////////////////////////////////////////////////
-// ==============
-// GAMEPLAY FUNKTIONEN
-// ==============
-//{///////////////////////////////////////////////////////////////////////////////////////
-
-//-------------------------------------------------------
-
-	////////////////////////////
-	// Die Funktion wenn die linke Maustaste gedrückt wird
-	public void LeftTrigger () {
-	
-		// Wird geprüft ob der maximale Ziehwert ungleich Null ist
-		if (maxPull != 0 & isBusy != true) {
-		
-			// Die Kollision mit der Kugel ist nun aktiv
-			collisionOn = true;
-			
-			// Switchabfrage für die Position des Cubes + die korrekte Berechnung der neuen Position
-			switch (location)
-			{
-			case 0:
-				StartCoroutine (GroundToTop ());
-				break;
-			
-			case 1:
-				StartCoroutine (TopToGround ());
-				break;
-
-			case 2:
-				StartCoroutine (FrontToBack ());
-				break;
-
-			case 3:
-				StartCoroutine (BackToFront ());
-				break;
-
-			case 4:
-				StartCoroutine (LeftToRight ());
-				break;
-
-			case 5:
-				StartCoroutine (RightToLeft ());
-				break;
-			}
-				
-			--maxPull;
-			++maxPush;
-
-		}
-	}
-
-//-------------------------------------------------------
-
-	////////////////////////////
-	// Die Funktion wenn die rechte Maustaste gedrückt wird
-	public void RightTrigger () {
-	
-		// Wird geprüft ob der maximale Drückwert ungleich Null ist
-		if (maxPush != 0 & isBusy != true) {
-
-			// Switchabfrage für die Position des Cubes + die korrekte Berechnung der neuen Position
-			switch (location)
-			{
-			case 0:
-				StartCoroutine (TopToGround ());
-				break;
-
-			case 1:
-				StartCoroutine (GroundToTop ());
-				break;
-
-			case 2:
-				StartCoroutine (BackToFront ());
-				break;
-
-			case 3:
-				StartCoroutine (FrontToBack ());
-				break;
-
-			case 4:
-				StartCoroutine (RightToLeft ());
-				break;
-
-			case 5:
-				StartCoroutine (LeftToRight ());
-				break;
-			}
-
-			++maxPull;
-			--maxPush;
-
-		}
-	}
+public class RedCube : MonoBehaviour
+{
 
-//-------------------------------------------------------
+    ////////////////////////////
+    // Alle Variablen
 
-//} ENDE GAMEPLAY FUNKTIONEN
+    // Öffentliche Floatvariablen für das maximale Ziehen und Drücken des Cubes + die Geschwindigkeit + die Kraft die ausgeübt auf der Kugel
+    public float maxPull = 0;
+    public float maxPush = 0;
+    public float speed = 1f;
+    public float force = 0f;
 
-//////////////////////////////////////////////////////////////////////////////////////////
-// ==============
-// COLLISION / TRIGGER FUNKTIONEN
-// ==============
-//{///////////////////////////////////////////////////////////////////////////////////////
+    // Öffentliche Intvariable für das Defenieren der Position des Cubes (0 = Boden; 1 = Decke; 2 = Vorne; 3 = Hinten; 4 = Links; 5 = Rechts)
+    public int location = 0;
 
-//-------------------------------------------------------
+    // Bool zum Prüfen ob der Cube allgemein eine Kollision mit Objekten haben soll
+    public bool haveCollision = false;
 
-	////////////////////////////
-	// Die Kollisionsabfrage mit der Kugel
-	void OnCollisionEnter (Collision col) {
+    // Startposition, Endposition und Distanz
+    private Vector3 startPos;
+    private Vector3 endPos;
+    private float distance = 1f;
 
-		// Wird geprüft ob der Cube überhaupt eine Kollision haben darf und ob diese aktiv ist
-		if (collisionOn != false && haveCollision != false) {
+    //Vektoren für From To deklarieren (initialiseren hier oben noch nicht möglich)
+    private Vector3 groundToTop;
+    private Vector3 topToGround;
+    private Vector3 frontToBack;
+    private Vector3 backToFront;
+    private Vector3 leftToRight;
+    private Vector3 rightToLeft;
 
-			// Switchabfrage für die Position des Cubes
-			switch (location)
-			{
-			case 0:
-				col.rigidbody.AddForce (Vector3.up * force);
-				break;
 
-			case 1:
-				col.rigidbody.AddForce (-Vector3.up * force);
-				break;
+    // Bool zum Überprüfen ob die Animation läuft und ob die Kollision aktiv oder inaktiv ist
+    private bool isBusy = false;
+    private bool collisionOn = false;
 
-			case 2:
-				col.rigidbody.AddForce (-Vector3.back * force);
-				break;
+    //-------------------------------------------------------
+
+    ////////////////////////////
+    // Use this for initialization
+    void Start()
+    {
 
-			case 3:
-				col.rigidbody.AddForce (Vector3.back * force);
-				break;
+        startPos = transform.position;
 
-			case 4:
-				col.rigidbody.AddForce (-Vector3.left * force);
-				break;
+        // initialiserie Vektoren
+        groundToTop = new Vector3(0, distance, 0);
+        topToGround = new Vector3(0, -distance, 0);
+        frontToBack = new Vector3(0, 0, -distance);
+        backToFront = new Vector3(0, 0, distance);
+        leftToRight = new Vector3(distance, 0, 0);
+        rightToLeft = new Vector3(-distance, 0, 0);
 
-			case 5:
-				col.rigidbody.AddForce (Vector3.left * force);
-				break;
-			}
-		}
-	}
+    }
 
-//-------------------------------------------------------
+    //-------------------------------------------------------
 
-//} ENDE COLLISION / TRIGGER FUNKTIONEN
+    ////////////////////////////
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
-//////////////////////////////////////////////////////////////////////////////////////////
-// ==============
-// IENUMERATOR / ANIMATION
-// ==============
-//{///////////////////////////////////////////////////////////////////////////////////////
+    //-------------------------------------------------------
 
-//-------------------------------------------------------
+    //////////////////////////////////////////////////////////////////////////////////////////
+    // ==============
+    // GAMEPLAY FUNKTIONEN
+    // ==============
+    //{///////////////////////////////////////////////////////////////////////////////////////
 
-	////////////////////////////
-	// Die Animation des Cubes von unten nach oben
-	IEnumerator GroundToTop () {
+    //-------------------------------------------------------
 
-		// Timer wird auf Null gesetzt und die Startposition und Endposition des Cubes wird ermittelt
-		float timeSinceStarted = 0f;
-		startPos = transform.position;
-		endPos = transform.position + new Vector3 (0, distance, 0);
+    ////////////////////////////
+    // Die Funktion wenn die linke Maustaste gedrückt wird
+    public void LeftTrigger()
+    {
 
-		// Der Timer wird gestartet
-		while (timeSinceStarted <= 1f) {
+        // Wird geprüft ob der maximale Ziehwert ungleich Null ist
+        if (maxPull != 0 & isBusy != true)
+        {
 
-			// Solange der Timer nicht zuende ist, ist der Cube "beschäftigt" und kann erstmal nicht weiter betätigt werden. Die Positionen vom Cube für die Animation werden berechnet und umgesetzt 
-			isBusy = true;
-			timeSinceStarted += Time.deltaTime * speed;
-			transform.position = Vector3.Lerp (startPos, endPos, timeSinceStarted);
-			yield return null;
+            // Die Kollision mit der Kugel ist nun aktiv
+            collisionOn = true;
 
-		}
+            // Switchabfrage für die Position des Cubes + die korrekte Berechnung der neuen Position
+            switch (location)
+            {
+                case 0: // GroundToTop
+                    StartCoroutine(Animate(groundToTop));
+                    break;
 
-		// Nachdem der Timer zuende ist, ist der Cube wieder interagierbar und die Kollision ist deaktiviert
-		isBusy = false;
-		yield return new WaitForSeconds (0.1f);
-		collisionOn = false;
+                case 1: // TopToGround
+                    StartCoroutine(Animate(topToGround));
+                    break;
 
-	}
+                case 2: // FrontToBack
+                    StartCoroutine(Animate(frontToBack));
+                    break;
 
-//-------------------------------------------------------
+                case 3: // BackToFront
+                    StartCoroutine(Animate(backToFront));
+                    break;
 
-	////////////////////////////
-	// Die Animation des Cubes von oben nach unten
-	IEnumerator TopToGround () {
+                case 4: // LeftToRight
+                    StartCoroutine(Animate(leftToRight));
+                    break;
 
-		float timeSinceStarted = 0f;
-		startPos = transform.position;
-		endPos = transform.position + new Vector3 (0, -distance, 0);
+                case 5: // RightToLeft
+                    StartCoroutine(Animate(rightToLeft));
+                    break;
+            }
+
+            --maxPull;
+            ++maxPush;
 
-		while (timeSinceStarted <= 1f) {
+        }
+    }
 
-			isBusy = true;
-			timeSinceStarted += Time.deltaTime * speed;
-			transform.position = Vector3.Lerp (startPos, endPos, timeSinceStarted);
-			yield return null;
+    //-------------------------------------------------------
 
-		}
+    ////////////////////////////
+    // Die Funktion wenn die rechte Maustaste gedrückt wird
+    public void RightTrigger()
+    {
 
-		isBusy = false;
-		yield return new WaitForSeconds (0.1f);
-		collisionOn = false;
+        // Wird geprüft ob der maximale Drückwert ungleich Null ist
+        if (maxPush != 0 & isBusy != true)
+        {
 
-	}
+            // Switchabfrage für die Position des Cubes + die korrekte Berechnung der neuen Position
+            switch (location)
+            {
+                case 0: // TopToGround
+                    StartCoroutine(Animate(topToGround));
+                    break;
+
+                case 1: // GroundToTop
+                    StartCoroutine(Animate(groundToTop));
+                    break;
+
+                case 2: // BackToFront
+                    StartCoroutine(Animate(backToFront));
+                    break;
+
+                case 3: // FrontToBack
+                    StartCoroutine(Animate(frontToBack));
+                    break;
+
+                case 4: // RightToLeft
+                    StartCoroutine(Animate(rightToLeft));
+                    break;
+
+                case 5: // LeftToRight
+                    StartCoroutine(Animate(leftToRight));
+                    break;
+            }
 
-//-------------------------------------------------------
+            ++maxPull;
+            --maxPush;
 
-	////////////////////////////
-	// Die Animation des Cubes von vorne nach hinten
-	IEnumerator FrontToBack () {
+        }
+    }
 
-		float timeSinceStarted = 0f;
-		startPos = transform.position;
-		endPos = transform.position + new Vector3 (0, 0, -distance);
+    //-------------------------------------------------------
 
-		while (timeSinceStarted <= 1f) {
+    //} ENDE GAMEPLAY FUNKTIONEN
 
-			isBusy = true;
-			timeSinceStarted += Time.deltaTime * speed;
-			transform.position = Vector3.Lerp (startPos, endPos, timeSinceStarted);
-			yield return null;
+    //////////////////////////////////////////////////////////////////////////////////////////
+    // ==============
+    // COLLISION / TRIGGER FUNKTIONEN
+    // ==============
+    //{///////////////////////////////////////////////////////////////////////////////////////
 
-		}
+    //-------------------------------------------------------
 
-		isBusy = false;
-		yield return new WaitForSeconds (0.1f);
-		collisionOn = false;
+    ////////////////////////////
+    // Die Kollisionsabfrage mit der Kugel
+    void OnCollisionEnter(Collision col)
+    {
 
-	}
+        // Wird geprüft ob der Cube überhaupt eine Kollision haben darf und ob diese aktiv ist
+        if (collisionOn != false && haveCollision != false)
+        {
 
-//-------------------------------------------------------
+            // Switchabfrage für die Position des Cubes
+            switch (location)
+            {
+                case 0:
+                    col.rigidbody.AddForce(Vector3.up * force);
+                    break;
 
-	////////////////////////////
-	// Die Animation des Cubes von hinten nach vorne
-	IEnumerator BackToFront () {
+                case 1:
+                    col.rigidbody.AddForce(-Vector3.up * force);
+                    break;
 
-		float timeSinceStarted = 0f;
-		startPos = transform.position;
-		endPos = transform.position + new Vector3 (0, 0, distance);
+                case 2:
+                    col.rigidbody.AddForce(-Vector3.back * force);
+                    break;
 
-		while (timeSinceStarted <= 1f) {
+                case 3:
+                    col.rigidbody.AddForce(Vector3.back * force);
+                    break;
 
-			isBusy = true;
-			timeSinceStarted += Time.deltaTime * speed;
-			transform.position = Vector3.Lerp (startPos, endPos, timeSinceStarted);
-			yield return null;
+                case 4:
+                    col.rigidbody.AddForce(-Vector3.left * force);
+                    break;
 
-		}
+                case 5:
+                    col.rigidbody.AddForce(Vector3.left * force);
+                    break;
+            }
+        }
+    }
 
-		isBusy = false;
-		yield return new WaitForSeconds (0.1f);
-		collisionOn = false;
+    //-------------------------------------------------------
 
-	}
+    //} ENDE COLLISION / TRIGGER FUNKTIONEN
 
-//-------------------------------------------------------
+    //////////////////////////////////////////////////////////////////////////////////////////
+    // ==============
+    // IENUMERATOR / ANIMATION
+    // ==============
+    //{///////////////////////////////////////////////////////////////////////////////////////
 
-	////////////////////////////
-	// Die Animation des Cubes von links nach rechts
-	IEnumerator LeftToRight () {
+    //-------------------------------------------------------
 
-		float timeSinceStarted = 0f;
-		startPos = transform.position;
-		endPos = transform.position + new Vector3 (distance, 0, 0);
 
-		while (timeSinceStarted <= 1f) {
+    ////////////////////////////
+    // Die Animation des Cubes, der Vektor fromTo gibt an, wo die Animation anfängt/ wo sie aufhört
+    IEnumerator Animate(Vector3 fromTo)
+    {
+        // Timer wird auf Null gesetzt und die Startposition und Endposition des Cubes wird ermittelt
+        float timeSinceStarted = 0f;
+        startPos = transform.position;
+        endPos = transform.position + fromTo;
 
-			isBusy = true;
-			timeSinceStarted += Time.deltaTime * speed;
-			transform.position = Vector3.Lerp (startPos, endPos, timeSinceStarted);
-			yield return null;
+        // Der Timer wird gestartet
+        while (timeSinceStarted <= 1f)
+        {
 
-		}
+            // Solange der Timer nicht zuende ist, ist der Cube "beschäftigt" und kann erstmal nicht weiter betätigt werden. Die Positionen vom Cube für die Animation werden berechnet und umgesetzt 
+            isBusy = true;
+            timeSinceStarted += Time.deltaTime * speed;
+            transform.position = Vector3.Lerp(startPos, endPos, timeSinceStarted);
+            yield return null;
 
-		isBusy = false;
-		yield return new WaitForSeconds (0.1f);
-		collisionOn = false;
+        }
 
-	}
+        // Nachdem der Timer zuende ist, ist der Cube wieder interagierbar und die Kollision ist deaktiviert
+        isBusy = false;
+        yield return new WaitForSeconds(0.1f);
+        collisionOn = false;
 
-//-------------------------------------------------------
+    }
 
-	////////////////////////////
-	// Die Animation des Cubes von rechts nach links
-	IEnumerator RightToLeft () {
 
-		float timeSinceStarted = 0f;
-		startPos = transform.position;
-		endPos = transform.position + new Vector3 (-distance, 0, 0);
-
-		while (timeSinceStarted <= 1f) {
-
-			isBusy = true;
-			timeSinceStarted += Time.deltaTime * speed;
-			transform.position = Vector3.Lerp (startPos, endPos, timeSinceStarted);
-			yield return null;
-
-		}
-
-		isBusy = false;
-		yield return new WaitForSeconds (0.1f);
-		collisionOn = false;
-
-	}
-
-//-------------------------------------------------------
-
-//} ENDE IENUMERATOR / ANIMATION
+    //} ENDE IENUMERATOR / ANIMATION
 }
